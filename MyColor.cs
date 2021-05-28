2e6a6deb461a4995e0b482855ea24d6002d2acbb
Gnidez <Gnidez@rivne.itstep.org>
2021-05-28 18:02:10 +0300

    first commit

---
+++ MyColor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfLesson6
+{
+    class MyColor
+    {
+        public Color AColor { get; set; }
+    }
+}
